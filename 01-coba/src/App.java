87199ea349a93810b2b6cd69e8a30f430a723f17
afdhalul404 <afdhalul3035@gmail.com>
2022-09-07 21:39:25 +0800

    integrasi ke git

---
+++ 01-coba/src/App.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        // System.out.println("Hello, World!");
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Masukan nama: ");
+        String nama = input.nextLine();
+
+        System.out.println("Nama anda: " + nama);
+
+
+
+    }
+}
